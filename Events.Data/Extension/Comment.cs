9249836eeb84bafa70dff5008614de710da38914
binnehaha321 <tqk792001@gmail.com>
2021-09-09 09:49:12 +0700

    update css

---
+++ Events.Data/Extension/Comment.cs
@@ -0,0 +1,6 @@
+﻿namespace Events.Data.Extension
+{
+    public class Comment
+    {
+    }
+}
--- Events.Data/Extension/CommentViewModel.cs
+++ Events.Data/Extension/CommentViewModel.cs
@@ -12,16 +12,10 @@ namespace Events.Data.Extension
         public string Text { get; set; }
         public string Author { get; set; }
 
-        public static Expression<Func<Comment, CommentViewModel>> ViewModel
+        public static Expression<Func<Comment, CommentViewModel>> ViewModel => c => new CommentViewModel()
         {
-            get
-            {
-                return c => new CommentViewModel()
-                {
-                    Text = c.Text,
-                    Author = c.Author.FullName
-                };
-            }
-        }
+            Text = c.Text,
+            Author = c.Author.FullName
+        };
     }
 }
